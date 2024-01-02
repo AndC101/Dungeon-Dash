4f6d0a907b36c5b0bdac3cd73af984ad0ed4b258
notjing <30694907+notjing@users.noreply.github.com>
2024-01-02 15:23:05 -0500

    e

--- Block.java
+++ Block.java
@@ -13,21 +13,11 @@ public class Block extends Rectangle {
 		super(x,y,l,w);
 		img = i;
 	}
-	
+
 	public Block(int x, int y, int l, int w){
 		super(x,y,l,w);
 	}
 	
-	// public static BufferedImage resize(BufferedImage img, int newW, int newH) {
-    //     Image tmp = img.getScaledInstance(newW, newH, Image.SCALE_SMOOTH);
-    //     BufferedImage dimg = new BufferedImage(newW, newH, BufferedImage.TYPE_INT_ARGB);
-
-    //     Graphics2D g2d = dimg.createGraphics();
-    //     g2d.drawImage(tmp, 0, 0, null);
-    //     g2d.dispose();
-
-    //     return dimg;
-    // }
 
 
 
@@ -54,5 +44,7 @@ public class Block extends Rectangle {
 		g.drawImage(img, x, y, width, height, null);
 	}
 
-
+	public String toString() {
+		return x + " " + y + " " + width + " " + height;
+	}
 }
