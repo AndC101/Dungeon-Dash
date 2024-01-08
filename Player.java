7af05433dd90bf2a5ac8d0822ed8848efd398203
notjing <30694907+notjing@users.noreply.github.com>
2024-01-08 13:18:01 -0500

    FIXED SCrollign kidna

--- Player.java
+++ Player.java
@@ -136,15 +136,11 @@ public class Player extends Rectangle {
     }
 
     public void draw(Graphics g) {
-        if( (left && isRight && x >= 450) || (!left && isLeft && x <= 450) ) {
+        if( (left && isRight && x >= 450) || (!left && isLeft && x <= 450)) {
             isCentered = true;
-            if(!done) {
-                xVelocity = 0;
-            } 
-            done = true;
+          
         }
-        
-        System.out.println(x + " " + isCentered);
+       
         
         
         g.setColor(Color.white);
