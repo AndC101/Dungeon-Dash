82dfd60f6d6c712af604138c6d049831ba205204
notjing <30694907+notjing@users.noreply.github.com>
2024-01-17 21:31:52 -0500

    comment

--- Projectile.java
+++ Projectile.java
@@ -11,28 +11,30 @@ import java.util.HashSet;
 
 public class Projectile extends Rectangle {
 
+
 	public static int height = 50;
 	public static int width = 50;
 	public boolean r, l = false;
 	public Image runL;
 	public Image runR;
 	public int shootDist = 400;
-	public int xBorder;
-	public int SPEED = 2;
-	public boolean isEnemy;
+	public int xBorder; //left or rightmost edge
+	public int SPEED = 2; //movement speed
+	public boolean isEnemy; //is an enemy? start moving
 	public int xVelocity;
 	public HashSet<Character> keysPressed = new HashSet<Character>();
 
+
 	public Projectile(int x, int y, int len, int w, Image left, Image right, boolean le, boolean ri, boolean enemy) throws IOException {
-		super(x, y, len, w);
+		super(x, y, len, w); //super class
 		runL = left;
 		runR = right;
 		l = le; 
 		r = ri;
 		if (enemy) {
-			xBorder = x;
+			xBorder = x; //initialize the border
 		}
-		isEnemy = enemy;
+		isEnemy = enemy; 
 	}
 
 	public void keyPressed(KeyEvent e) {
@@ -42,10 +44,10 @@ public class Projectile extends Rectangle {
 	public void keyReleased(KeyEvent e) {
 		// check for errors
 		keysPressed.remove(e.getKeyChar());
-		// System.out.println(keysPressed + " " + keysPressed.size());
 
 		if(e.getKeyChar() == 'a' || e.getKeyChar() == 'd') {
 
+			//updates speeds when the keys are released
 			if(e.getKeyChar() == 'a') {
 				if(l) {
 					setXDirection(-SPEED);
@@ -64,14 +66,11 @@ public class Projectile extends Rectangle {
 		}
 	}
 
-	
-
 	// draw the image from the block class
 	public void draw(Graphics2D g, int yTur) {
 		if(isEnemy) {
-			// g.setColor(Color.red);
+			//updates the sprite
 			if (r) {
-				// g.fillRect(xBorder-GamePanel.shift-20, 40, 2, 1000); //debugging
 				g.drawImage(runR, x, yTur-10, null);
 			} else if (l) {
 				// g.fillRect(xBorder-GamePanel.shift-Turret.width, 40, 2, 1000); //debugging
@@ -87,6 +86,7 @@ public class Projectile extends Rectangle {
 	}
 
 	public void move() {
+		//moves the projectile at a relative speed to blocks which makes it seem like moving
 		if (keysPressed.contains('d')) {
 			if (Player.isCentered && Player.isRight) {
 
@@ -99,10 +99,14 @@ public class Projectile extends Rectangle {
 				// xBorder+=-5;
 	
 			}
+
+
+			//if edit, it moves at block speed
 			if(GamePanel.edit){
 				x-=Block.SPEED;
 			}
 
+			//same logic but for left key
 		} else if (keysPressed.contains('a')) {
 			if (Player.isCentered && Player.isLeft) {
 
@@ -115,7 +119,6 @@ public class Projectile extends Rectangle {
 				// xBorder+=5;
 			}
 			if(GamePanel.edit){
-				System.out.println("aaaaaaaaaaaaaaaaaaa");
 				x+=Block.SPEED;
 			}
 
@@ -130,10 +133,9 @@ public class Projectile extends Rectangle {
 			}
 		}
 
-		// System.out.println(x);
-
 		x += xVelocity;
 
+		//makes projectile move left to right or vice versa. makes it appear to shoot from turret
 		if (isEnemy) {
 			if(r) {
 				if (x <= xBorder - GamePanel.shift-20) {
