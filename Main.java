649200f4f378cd0e5cb0ba3a424a7e50a7c0e09b
Andrew Chen <andrew.chen7@student.tdsb.on.ca>
2024-01-17 21:27:26 -0500

    comments

--- Main.java
+++ Main.java
@@ -1,8 +1,8 @@
-/* Main class starts the game
-All it does is run the constructor in GameFrame class
-
-This is a common technique among coders to keep things organized (and handy when coding in repl.it since we're forced to call a class Main, which isn't always very descriptive)
-*/ 
+/*
+ * Ethan Lin & Andrew Chen
+ * January 11, 2023
+ * Main runs the program
+ */
 
 import java.io.IOException;
 
